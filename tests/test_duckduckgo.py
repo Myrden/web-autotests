4c8df4e2a3d13ce27c22ed9dd01ad6b385e6f28d
Myrden <mirden.didie@gmail.com>
2020-12-08 18:34:29 +0200

    Add test for duckduckgo

---
+++ tests/test_duckduckgo.py
@@ -0,0 +1,12 @@
+from selene import have
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s, ss
+
+
+def test_find():
+    browser.open('https://duckduckgo.com/')
+
+    s('[name=q]').type('yashaka selene').press_enter()
+    ss('.result__body').first.click()
+
+    ss('[href="/yashaka/selene"]').should(have.size(3))
